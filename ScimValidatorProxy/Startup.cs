6fa231d829f3998e581e1d7b6f33d7f6966b79a9
Geoff Bones <Geoff.Bones@siemens.com>
2023-06-06 12:18:47 +0100

    Remove scim stuff from proxy

--- ScimValidatorProxy/Startup.cs
+++ ScimValidatorProxy/Startup.cs
@@ -2,7 +2,6 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //------------------------------------------------------------
 
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -10,7 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.SCIM;
 using ScimValidatorProxy.Controllers;
-using ScimValidatorProxy.Provider;
 
 namespace ScimValidatorProxy
 {
@@ -26,7 +24,6 @@ namespace ScimValidatorProxy
             this.environment = env;
 
             this.MonitoringBehavior = new ConsoleMonitor();
-            this.ProviderBehavior = new InMemoryProvider();
         }
 
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
