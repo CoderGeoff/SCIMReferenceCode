74dfe8bcefdc9fc5d7b1024e343db833f0e92d50
Geoff Bones <Geoff.Bones@siemens.com>
2023-06-06 12:19:06 +0100

    Tidy

--- ScimValidatorProxy/Program.cs
+++ ScimValidatorProxy/Program.cs
@@ -11,7 +11,7 @@ namespace ScimValidatorProxy
     {
         public static void Main(string[] args)
         {
-            Program.CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- ScimValidatorProxy/Startup.cs
+++ ScimValidatorProxy/Startup.cs
@@ -1,10 +1,5 @@
-//------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//------------------------------------------------------------
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.SCIM;
@@ -19,25 +14,24 @@ namespace ScimValidatorProxy
         public IMonitor MonitoringBehavior { get; set; }
         public IProvider ProviderBehavior { get; set; }
 
-        public Startup(IWebHostEnvironment env, IConfiguration configuration)
+        public Startup(IWebHostEnvironment env)
         {
-            this.environment = env;
-
-            this.MonitoringBehavior = new ConsoleMonitor();
+            environment = env;
+            MonitoringBehavior = new ConsoleMonitor();
         }
 
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(typeof(IProvider), this.ProviderBehavior)
-                .AddSingleton(typeof(IMonitor), this.MonitoringBehavior)
+            services.AddSingleton(typeof(IProvider), ProviderBehavior)
+                .AddSingleton(typeof(IMonitor), MonitoringBehavior)
                     .AddSingleton(typeof(IProxy), new Proxy("49975", "49977"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            if (this.environment.IsDevelopment())
+            if (environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
